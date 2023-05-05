6512d61e89bf870673edf8d635c453a9dbf094c2
“[firstname <clarknaomi7@gmail.com>
2023-05-05 04:40:44 -0500

    edited pokemon module

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,14 +1,5 @@
 const {Schema, model} = require("mongoose");
 
-const pokemondb = [
-  {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
-  {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
-  {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
-  {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
-  {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
-  {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
-  {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}
-];
 
 //creating a new schema
 const pokemonSchema = new Schema({
